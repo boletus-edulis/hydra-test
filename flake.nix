7cad577c1ab8733989a08fe512003300ebf13f09
boletus-edulis <the.boletus.edulis@gmail.com>
2023-10-28 23:18:54 +0200

    hdhdh

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.iosevka-term = pkgs.iosevka.override { set = "term"; };
-        packages.firefox = pkgs.firefox;
-      });
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+    in {
+      packages.iosevka-term = pkgs.iosevka.override { set = "term"; };
+    };
 }
