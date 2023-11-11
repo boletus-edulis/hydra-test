405e60b5492b1d915dc134b44936c935ae03d6ad
boletus-edulis <the.boletus.edulis@gmail.com>
2023-11-11 02:34:33 +0100

    test without kernel

--- flake.nix
+++ flake.nix
@@ -21,28 +21,30 @@
         iosevka-term = forAllSystems (system:
           nixpkgs.legacyPackages.${system}.iosevka.override {
             set = "term";
-          });
+          }
+        );
         firefox = forAllSystems (system:
           nixpkgs.legacyPackages.${system}.firefox
         );
-        linux_6_6_x13s = forAllSystems (system:
-          nixpkgs.legacyPackages.${system}.linux_testing.override {
-            argsOverride = rec {
-              version = "6.6";
-              modDirVersion = "6.6.0";
-              extraMeta.branch = lib.versions.majorMinor version;
-              #modDirVersion = lib.versions.pad 3 version;
-
-              src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
-                owner = "steev";
-                repo = "linux";
-                rev = "lenovo-x13s-linux-6.6.y";
-                sha256 = "sha256-Pa89+GqmVrs0oZ/ALz23XUjUYjHSd7zoWoxF5ipsM90=";
-              };
-
-              defconfig = "laptop_defconfig";
-            };
-          });
+        #linux_6_6_x13s = forAllSystems (system:
+        #  nixpkgs.legacyPackages.${system}.linux_testing.override {
+        #    argsOverride = rec {
+        #      version = "6.6";
+        #      modDirVersion = "6.6.0";
+        #      extraMeta.branch = lib.versions.majorMinor version;
+        #      #modDirVersion = lib.versions.pad 3 version;
+        #
+        #      src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+        #        owner = "steev";
+        #        repo = "linux";
+        #        rev = "lenovo-x13s-linux-6.6.y";
+        #        sha256 = "sha256-Pa89+GqmVrs0oZ/ALz23XUjUYjHSd7zoWoxF5ipsM90=";
+        #      };
+        #
+        #      defconfig = "laptop_defconfig";
+        #    };
+        #  }
+        #);
       };
     };
 }
