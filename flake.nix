45c4a969594284e5596b16685ff3cbacd4c3c991
boletus-edulis <the.boletus.edulis@gmail.com>
2023-11-11 02:34:33 +0100

    test kernel

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs, ... } @ inputs:
     let
       supportedSystems = [
-        "x86_64-linux"
+        #"x86_64-linux"
         "aarch64-linux"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -26,25 +26,25 @@
         firefox = forAllSystems (system:
           nixpkgs.legacyPackages.${system}.firefox
         );
-        #linux_6_6_x13s = forAllSystems (system:
-        #  nixpkgs.legacyPackages.${system}.linux_testing.override {
-        #    argsOverride = rec {
-        #      version = "6.6";
-        #      modDirVersion = "6.6.0";
-        #      extraMeta.branch = lib.versions.majorMinor version;
-        #      #modDirVersion = lib.versions.pad 3 version;
-        #
-        #      src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
-        #        owner = "steev";
-        #        repo = "linux";
-        #        rev = "lenovo-x13s-linux-6.6.y";
-        #        sha256 = "sha256-Pa89+GqmVrs0oZ/ALz23XUjUYjHSd7zoWoxF5ipsM90=";
-        #      };
-        #
-        #      defconfig = "laptop_defconfig";
-        #    };
-        #  }
-        #);
+        linux_6_6_x13s = forAllSystems (system:
+          nixpkgs.legacyPackages.${system}.linux_testing.override {
+            argsOverride = rec {
+              version = "6.6";
+              modDirVersion = "6.6.0";
+              extraMeta.branch = lib.versions.majorMinor version;
+              #modDirVersion = lib.versions.pad 3 version;
+
+              src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+                owner = "steev";
+                repo = "linux";
+                rev = "lenovo-x13s-linux-6.6.y";
+                sha256 = "sha256-Pa89+GqmVrs0oZ/ALz23XUjUYjHSd7zoWoxF5ipsM90=";
+              };
+
+              defconfig = "laptop_defconfig";
+            };
+          }
+        );
       };
     };
 }
