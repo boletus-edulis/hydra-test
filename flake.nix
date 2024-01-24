955273384ecf80df9d51d0e5be8291cf34018a1b
boletus-edulis <the.boletus.edulis@gmail.com>
2024-01-24 23:04:43 +0100

    more platform packages

--- flake.nix
+++ flake.nix
@@ -12,22 +12,123 @@
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages;
       buildPackageAttrs = pkglist:
         forAllSystems (system:
           (lib.attrsets.genAttrs pkglist
-            (name: nixpkgs.legacyPackages.${system}.${name})));
+            (name: pkgs.${system}.${name})));
     in
     rec {
       packages = forAllSystems (system: (buildPackageAttrs [
         "thunderbird"
         "firefox"
         "scribus"
-      ]).${system} // {
-        iosevka-term = nixpkgs.legacyPackages.${system}.iosevka.override { set = "term"; };
-        linux_6_7_x13s = nixpkgs.legacyPackages.${system}.linux_6_7.override {
+      ]).${system} // rec {
+        x13s-firmware = (pkgs.${system}.stdenv.mkDerivation {
+          pname = "x13s-firmware";
+          version = "9999";
+
+          src = pkgs.${system}.fetchFromGitHub {
+            owner = "ironrobin";
+            repo = "x13s-alarm";
+            rev = "2dcb4c8";
+            sha256 = "sha256-SbqXeU2pjPb4tpDU4/UqBZ8Ev+J6P9qCO5g6no5WFwM=";
+          };
+
+          sourceRoot = "./source";
+          dontBuild = true;
+          # Firmware blobs do not need fixing and should not be modified
+          dontFixup = true;
+
+          nativeBuildInputs = [ pkgs.${system}.linux-firmware ];
+
+          installPhase = ''
+            runHook preInstall
+
+            cp -vaL "${pkgs.${system}.linux-firmware}/" "$out/"
+            chmod -R +w "$out/"
+
+            mkdir -p "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX"
+            cp -fv "x13s-firmware/qcvss8280.mbn" \
+               "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX"
+
+            mkdir -p "$out/lib/firmware/qca"
+            cp -fv "x13s-firmware/hpnv21.b8c" "$out/lib/firmware/qca"
+            cp -fv "x13s-firmware/a690_gmu.bin" "$out/lib/firmware/qcom"
+            # cp -fv "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX/audioreach-tplg.bin" \
+            #    "$out/lib/firmware/qcom/sc8280xp/SC8280XP-LENOVO-X13S-tplg.bin"
+
+            runHook postInstall
+          '';
+
+          meta = with lib; {
+            description = "Firmware for Lenovo Thinkpad X13s";
+            homepage = "https://github.com/ironrobin/x13s-alarm";
+            license = licenses.unfreeRedistributableFirmware;
+            platforms = platforms.linux;
+          };
+        });
+        qrtr = (pkgs.${system}.stdenv.mkDerivation rec {
+          pname = "qrtr";
+          version = "d0d471c";
+
+          src = pkgs.${system}.fetchFromGitHub {
+            owner = "andersson";
+            repo = "qrtr";
+            rev = "${version}";
+            sha256 = "sha256-KF0gCBRw3BDJdK1s+dYhHkokVTHwRFO58ho0IwHPehc=";
+          };
+
+          makeFlags = [
+            "CC=gcc"
+            "prefix=$(out)"
+          ];
+
+          meta = with lib; {
+            description = "qrtr";
+            homepage = "https://github.com/andersson/qrtr";
+            license = licenses.unfreeRedistributableFirmware;
+            platforms = platforms.linux;
+          };
+        });
+        pd-mapper = (pkgs.${system}.stdenv.mkDerivation rec {
+          pname = "pd-mapper";
+          version = "107104b";
+
+          src = pkgs.${system}.fetchFromGitHub {
+            owner = "andersson";
+            repo = "pd-mapper";
+            rev = "${version}";
+            sha256 = "sha256-ypLS/g1FNi2vzIYkIoml2FkMM1Tc8UrRRhWaYbwpwkc=";
+          };
+
+          nativeBuildInputs = [ x13s-firmware qrtr ];
+
+          preBuild = ''
+            sed -i 's|/lib/firmware/|${
+              #(pkgs.callPackage ./firmware { })
+              x13s-firmware
+            }/lib/firmware/|g' pd-mapper.c
+            grep "/lib/firmware/" pd-mapper.c
+          '';
+
+          makeFlags = [
+            "CC=gcc"
+            "prefix=$(out)"
+          ];
+
+          meta = with lib; {
+            description = "pd-mapper";
+            homepage = "https://github.com/andersson/pd-mapper";
+            license = licenses.unfreeRedistributableFirmware;
+            platforms = platforms.linux;
+          };
+        });
+        iosevka-term = pkgs.${system}.iosevka.override { set = "term"; };
+        linux_6_7_x13s = pkgs.${system}.linux_6_7.override {
           argsOverride = rec {
             modDirVersion = "6.7.1";
-            src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+            src = pkgs.${system}.fetchFromGitHub {
               owner = "steev";
               repo = "linux";
               rev = "lenovo-x13s-linux-6.7.y";
