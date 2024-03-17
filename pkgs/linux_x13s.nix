c103a9bf7869cb0d7516562ce4b97186bd462d89
boletus-edulis <the.boletus.edulis@gmail.com>
2024-03-17 16:01:28 +0100

    update linux

--- pkgs/linux_x13s.nix
+++ pkgs/linux_x13s.nix
@@ -3,11 +3,16 @@
 , lib
 , ...
 }:
-pkgs.linux_6_8.override {
+let
+  linux = pkgs.linux_6_8;
+  modDirVersion = pkgs.linux_6_8.modDirVersion;
+in
+linux.override {
   argsOverride = {
     inherit src;
     #modDirVersion = lib.versions.pad 3 pkgs.linux_6_7.version;
-    modDirVersion = "6.8.1";
+    inherit modDirVersion;
+    #"6.8.1";
     defconfig = "laptop_defconfig";
     #defconfig = "johan_defconfig";
     structuredExtraConfig = with lib.kernel; {
@@ -22,15 +27,24 @@ pkgs.linux_6_8.override {
       KEYBOARD_GPIO = yes;
       SERIAL_QCOM_GENI = yes;
       PINCTRL_QCOM_SPMI_PMIC = yes;
-      PINCTRL_SC8280XP_LPASS_LPI = module;
       BACKLIGHT_CLASS_DEVICE = yes;
       VIRTIO_MENU = yes;
       VHOST_MENU = yes;
       SC_GCC_8280XP = yes;
       SC_GPUCC_8280XP = yes;
-      QCOM_Q6V5_ADSP = module;
       QCOM_CPR = yes;
+      NVMEM_QCOM_QFPROM = yes;
+      CRYPTO_AES_ARM64_CE_BLK = yes;
+      CRYPTO_AES_ARM64_BS = yes;
+      CRYPTO_AES_ARM64_CE_CCM = yes;
+
+      QCOM_RPMPD = module;
+      QCOM_TSENS = module;
+      QCOM_STATS = module;
+      QCOM_RPMHPD = module;
+      QCOM_Q6V5_ADSP = module;
       PHY_QCOM_QMP_PCIE_8996 = module;
+      PINCTRL_SC8280XP_LPASS_LPI = module;
     };
   };
 }
