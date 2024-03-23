51da5e4d4a3c6398c2568965dc0898f34031ef8e
boletus-edulis <the.boletus.edulis@gmail.com>
2024-03-23 12:46:17 +0100

    try again without extra config

--- pkgs/linux_x13s.nix
+++ pkgs/linux_x13s.nix
@@ -15,36 +15,36 @@ linux.override {
     #"6.8.1";
     #defconfig = "laptop_defconfig";
     configfile = ./laptop_defconfig;
-    structuredExtraConfig = with lib.kernel; {
-      VIDEO_AR1337 = no;
-      AUDIT = yes;
-      ARM64_SME = yes;
-      MAC80211_LEDS = yes;
-      FW_LOADER_USER_HELPER = yes;
-      QCOM_EBI2 = yes;
-      EFI_CAPSULE_LOADER = yes;
-      SRAM = yes;
-      KEYBOARD_GPIO = yes;
-      SERIAL_QCOM_GENI = yes;
-      PINCTRL_QCOM_SPMI_PMIC = yes;
-      BACKLIGHT_CLASS_DEVICE = yes;
-      VIRTIO_MENU = yes;
-      VHOST_MENU = yes;
-      SC_GCC_8280XP = yes;
-      SC_GPUCC_8280XP = yes;
-      QCOM_CPR = yes;
-      NVMEM_QCOM_QFPROM = yes;
-      CRYPTO_AES_ARM64_CE_BLK = yes;
-      CRYPTO_AES_ARM64_BS = yes;
-      CRYPTO_AES_ARM64_CE_CCM = yes;
-
-      #QCOM_RPMPD = module;
-      #QCOM_TSENS = module;
-      #QCOM_STATS = module;
-      #QCOM_RPMHPD = module;
-      #QCOM_Q6V5_ADSP = module;
-      #PHY_QCOM_QMP_PCIE_8996 = module;
-      #PINCTRL_SC8280XP_LPASS_LPI = module;
-    };
+#    structuredExtraConfig = with lib.kernel; {
+#      VIDEO_AR1337 = no;
+#      AUDIT = yes;
+#      ARM64_SME = yes;
+#      MAC80211_LEDS = yes;
+#      FW_LOADER_USER_HELPER = yes;
+#      QCOM_EBI2 = yes;
+#      EFI_CAPSULE_LOADER = yes;
+#      SRAM = yes;
+#      KEYBOARD_GPIO = yes;
+#      SERIAL_QCOM_GENI = yes;
+#      PINCTRL_QCOM_SPMI_PMIC = yes;
+#      BACKLIGHT_CLASS_DEVICE = yes;
+#      VIRTIO_MENU = yes;
+#      VHOST_MENU = yes;
+#      SC_GCC_8280XP = yes;
+#      SC_GPUCC_8280XP = yes;
+#      QCOM_CPR = yes;
+#      NVMEM_QCOM_QFPROM = yes;
+#      CRYPTO_AES_ARM64_CE_BLK = yes;
+#      CRYPTO_AES_ARM64_BS = yes;
+#      CRYPTO_AES_ARM64_CE_CCM = yes;
+#
+#      #QCOM_RPMPD = module;
+#      #QCOM_TSENS = module;
+#      #QCOM_STATS = module;
+#      #QCOM_RPMHPD = module;
+#      #QCOM_Q6V5_ADSP = module;
+#      #PHY_QCOM_QMP_PCIE_8996 = module;
+#      #PINCTRL_SC8280XP_LPASS_LPI = module;
+#    };
   };
 }
