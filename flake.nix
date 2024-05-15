970dcf3889392b83d0c48b5bc1eb2bbaa1c137c6
boletus-edulis <the.boletus.edulis@gmail.com>
2024-05-15 22:10:38 +0200

    update linux

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     };
 
     linux-jhovold-6-9 = {
-      url = "git+https://github.com/jhovold/linux?ref=wip/sc8280xp-6.9-rc7";
+      url = "git+https://github.com/jhovold/linux?ref=wip/sc8280xp-6.9";
       flake = false;
     };
 
