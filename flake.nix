be69915f28a45347ffa62bd9d557d2718737832a
boletus-edulis <the.boletus.edulis@gmail.com>
2025-07-20 22:08:22 +0200

    use release version again

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
             linux_x13s = let
               linux_version = import ./linux_version.nix;
             in pkgs.callPackage ./pkgs/linux_x13s.nix {
-              src = inputs.linux-steeve-6-16-rc5;
+              src = inputs.linux-steeve-6-15;
               version = linux_version;
               defconfig = "johan_defconfig";
             };
